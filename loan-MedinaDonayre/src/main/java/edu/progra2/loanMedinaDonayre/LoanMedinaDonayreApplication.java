daab73e99cff7d8f026ab2c66f8361df5652851d
Clase <Clase@1A-PC27>
2018-04-21 09:26:31 -0500

    first commit

---
+++ loan-MedinaDonayre/src/main/java/edu/progra2/loanMedinaDonayre/LoanMedinaDonayreApplication.java
@@ -0,0 +1,12 @@
+package edu.progra2.loanMedinaDonayre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMedinaDonayreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMedinaDonayreApplication.class, args);
+	}
+}
---
+++ loan-MedinaDonayre/src/test/java/edu/progra2/loanMedinaDonayre/LoanMedinaDonayreApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.progra2.loanMedinaDonayre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoanMedinaDonayreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
