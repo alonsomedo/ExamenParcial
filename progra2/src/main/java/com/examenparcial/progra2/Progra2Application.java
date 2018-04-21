8fefab8b14f7713bfbd9cd48f16cbd06c4442ee1
Clase <Clase@1A-PC27>
2018-04-21 08:59:22 -0500

    1wgwf

---
+++ progra2/src/main/java/com/examenparcial/progra2/Progra2Application.java
@@ -0,0 +1,12 @@
+package com.examenparcial.progra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progra2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progra2Application.class, args);
+	}
+}
